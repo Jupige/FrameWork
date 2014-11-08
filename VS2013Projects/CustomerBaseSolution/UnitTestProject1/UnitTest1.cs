051b3922706860e12b3c90d7671ff43db8de2408
Jupige <pig567abc@126.com>
2014-11-08 12:03:00 +0800

    5555

---
+++ VS2013Projects/CustomerBaseSolution/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using CB.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static ICBService _icbService ;
+
+        private static  ICBService ClientService
+        {
+            get
+            {
+                if (_icbService == null)
+                {
+
+                   var channelFactory = new ChannelFactory<ICBService>("CB.Service.ICBService");
+                    
+                   _icbService = channelFactory.CreateChannel();
+                    
+                }
+                return _icbService;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClientService.SayHello();
+
+            //ClientService.SayHello();
+        }
+    }
+}
